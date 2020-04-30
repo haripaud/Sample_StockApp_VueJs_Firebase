32e0c8c92041c97b95b20569cc26adacc87887c9
haripaud <p.hary804@gmail.com>
2020-04-30 15:07:55 -0400

    Initial commit

---
+++ src/components/portfolio/Stock.vue
@@ -0,0 +1,89 @@
+<template>
+    <div>
+        <div class="col-sm-6 col-md-4">
+            <div class="panel panel-info">
+                <div class="panel-heading">
+                    <h3 class="panel-title">
+                        {{ stock.name }}
+                        <small>(Price: {{ stock.price }} | Quantity: {{ stock.quantity }})</small>
+                    </h3>
+                </div>
+                <div class="panel-body">
+                    <div class="pull-left">
+                        <input
+                                type="number"
+                                class="form-control"
+                                placeholder="Quantity"
+                                v-model="quantity"
+                                :class="{danger: insufficientQuantity}"
+                        >
+                    </div>
+                    <div class="pull-right">
+                        <button
+                                class="btn btn-danger"
+                                @click="sellStock"
+                        >{{ insufficientQuantity ? 'Not enough' : 'Sell' }}
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="alert alert-info col-sm-12 col-md-12" role="alert" v-if="purchased.flag">
+            You have sold {{this.purchased.order.quantity}} units @{{this.purchased.order.stockPrice}} price successfully!
+        </div>
+    </div>
+</template>
+
+<style scoped>
+    .danger {
+        border: 1px solid red;
+    }
+
+    .alert-info {
+        background-color: green !important;
+        color: #fff;
+        text-align: center;
+        font-weight: bold;
+    }
+</style>
+
+<script>
+    import {mapActions} from 'vuex';
+
+    export default {
+        props: ['stock'],
+        data() {
+            return {
+                quantity: 0,
+                purchased: {
+                    flag: false,
+                    order: null
+                }
+            }
+        },
+        computed: {
+          insufficientQuantity() {
+              return this.quantity > this.stock.quantity;
+          }
+        },
+        methods: {
+            ...mapActions({
+                placeSellOrder: 'sellStock'
+            }),
+            sellStock() {
+                const order = {
+                    stockId: this.stock.id,
+                    stockPrice: this.stock.price,
+                    quantity: this.quantity
+                };
+                this.placeSellOrder(order);
+                this.purchased.flag = true;
+                this.purchased.order = {...order};
+                setTimeout(() => {
+                    this.purchased.flag = false;
+                }, 3000);
+                this.quantity = 0;
+            }
+        }
+    }
+</script>
---
+++ src/components/stocks/Stock.vue
@@ -0,0 +1,89 @@
+<template>
+    <div>
+        <div class="col-sm-6 col-md-4">
+            <div class="panel panel-info">
+                <div class="panel-heading">
+                    <h3 class="panel-title">
+                        {{ stock.name }}
+                        <small>(Price: {{ stock.price }})</small>
+                    </h3>
+                </div>
+                <div class="panel-body">
+                    <div class="pull-left">
+                        <input
+                                type="number"
+                                class="form-control"
+                                placeholder="Quantity"
+                                v-model="quantity"
+                                :class="{danger: insufficientFunds}"
+                        >
+                    </div>
+                    <div class="pull-right">
+                        <button
+                                class="btn btn-success"
+                                @click="buyStock"
+                        >{{ insufficientFunds ? 'Insufficient Funds' : 'Buy' }}
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="alert alert-info col-sm-12 col-md-12" role="alert" v-if="purchased.flag">
+            You have purchased {{this.purchased.order.quantity}} units @{{this.purchased.order.stockPrice}} price successfully!
+        </div>
+    </div>
+</template>
+
+<style scoped>
+    .danger {
+        border: 1px solid red;
+    }
+
+    .alert-info {
+        background-color: green !important;
+        color: #fff;
+        text-align: center;
+        font-weight: bold;
+    }
+
+</style>
+
+<script>
+    export default {
+        props: ['stock'],
+        data() {
+            return {
+                quantity: 0,
+                purchased: {
+                    flag: false,
+                    order: null
+                }
+            }
+        },
+        computed: {
+            funds() {
+                return this.$store.getters.funds;
+            },
+            insufficientFunds() {
+                return this.quantity * this.stock.price > this.funds;
+            }
+        },
+        methods: {
+            buyStock() {
+                const order = {
+                    stockId: this.stock.id,
+                    stockPrice: this.stock.price,
+                    quantity: this.quantity
+                };
+                this.$store.dispatch('buyStock', order).then(() => {
+                    this.purchased.flag = true;
+                    this.purchased.order = {...order};
+                    setTimeout(() => {
+                        this.purchased.flag = false;
+                    }, 3000);
+                });
+                this.quantity = 0;
+            }
+        }
+    }
+</script>
